9089612862198a71917b22806f50dd469c00daf4
NIKHLESH MAHATO <134185273+nikhleshmahato@users.noreply.github.com>
2026-09-14 15:58:22 +0530

    Fix onClick syntax for buttons in function.jsx
    Updated button onClick syntax for proper function calls.

--- Components/function.jsx
+++ Components/function.jsx
@@ -12,11 +12,11 @@ function App(){
             <button onClick({fruit()})>click </button> 
             // this statement will call the function as the page load without click
             
-            <button onClick({fruit})>click </button> 
+            <button onClick = {fruit}>click </button> 
             //in react, onclick need direct function declaration 
 
             // Then how to pass parameter inside function 
-            <button onClick({()=>animal("banana")})>click</button>
+            <button onClick = {()=>animal("banana")}>click</button>
             // this works because onClick is executing the arrow function which has a child function animal.
             // so it is not diretly calling , it running the arrow function declaration , which is then calling the function.
         </div>
