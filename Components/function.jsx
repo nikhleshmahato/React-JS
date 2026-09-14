2a744dc24be94a61e64828c054e5cc9a8c6153aa
NIKHLESH MAHATO <134185273+nikhleshmahato@users.noreply.github.com>
2026-09-14 13:57:32 +0530

    Add App component with alert buttons
    Implement App component with buttons for alerts and function calls.

---
+++ Components/function.jsx
@@ -0,0 +1,64 @@
+function App(){
+    function fruit(){
+        alert("Apple");
+    }
+
+    const animal = (name) => {
+        alert(name)
+    }
+
+    return 
+        <div>
+            <button onClick({fruit()})>click</button> // this statement will call the function as the page load without click
+            
+            <button onClick({fruit})>click </button> //in react, onclick need direct function declaration 
+
+            // Then how to pass parameter inside function 
+            <button onClick({()=>animal("banana")})>click</button>
+            // this works because onClick is executing the arrow function which has a child function animal.
+            // so it is not diretly calling , it running the arrow function declaration , which is then calling the function.
+        </div>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// react uses camelCase for built-in functions.
+// ex : 
+// in js - onclick()
+// in react - onClick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
